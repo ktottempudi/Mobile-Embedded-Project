d4ac932c68d42c9d4e8f788f308609db92b5fdc3
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2019-12-15 18:29:27 -0500

    Print Picture with all Sensors

--- AllSensors.py
+++ AllSensors.py
@@ -3,6 +3,7 @@
 import pigpio
 import RPi.GPIO as GPIO
 import time
+import os
 from gpiozero import LightSensor, Buzzer
 
 
@@ -27,46 +28,45 @@ pi.bb_serial_read_open(RX, 115200)
 LidarDistance = 0
 LightValue = 0
 UltrasonicDistance = 0
+prevUltraDist = 0
+prevLidVal = 0
+prevLight = 0
 
 def getTFminiData():
-	while True:
-		#print("#############")
-		#time.sleep(0.05)	#change the value if needed
-		time.sleep(1)
-		(count, recv) = pi.bb_serial_read(RX)
-		if count > 8:
-			for i in range(0, count-9):
-				if recv[i] == 89 and recv[i+1] == 89: # 0x59 is 89
-					checksum = 0
-					for j in range(0, 8):
-						checksum = checksum + recv[i+j]
-					checksum = checksum % 256
-					if checksum == recv[i+8]:
-						UltrasonicDistance = recv[i+2] + recv[i+3] * 256
-						strength = recv[i+4] + recv[i+5] * 256
-						if UltrasonicDistance <= 1200 and strength < 2000:
-							if count>0 and prev !=distance
-								camera =picamera.PiCamera()
-								camera.resolution=(800,600)
-								camera.start_preview()
-								sleep(1)
-								camera.capture('snapshot'+str(imcount)+'.jpg',resize=(640,480))
-								camera.stop_preview()
-							#print(distance, strength) 
-							imcount =imcount+1
-							prev=distance
-						#else:
-							# raise ValueError('distance error: %d' % distance)	
-						#i = i + 9
+    while True:
+        #print("#############")
+        #time.sleep(0.05)   #change the value if needed
+        time.sleep(1)
+        (count, recv) = pi.bb_serial_read(RX)
+        if count > 8:
+            for i in range(0, count-9):
+                if recv[i] == 89 and recv[i+1] == 89: # 0x59 is 89
+                    checksum = 0
+                    for j in range(0, 8):
+                        checksum = checksum + recv[i+j]
+                    checksum = checksum % 256
+                    if checksum == recv[i+8]:
+                        #LidarDistance = recv[i+2] + recv[i+3] * 256
+                        dist = recv[i+2] + recv[i+3] * 256
+                        #strength = recv[i+4] + recv[i+5] * 256
+                        if(abs(dist-prevLidVal)>5):
+                            LidarDistance=dist
+                            print(LidarDistance)
+                            prevLidVal=dist
+                        else:
+                            prevLidVal=dist 
+                        imcount =imcount+1
+                        prev=distance
+                        #else:
+                            # raise ValueError('distance error: %d' % distance) 
+                        #i = i + 9
 
 def getUltraSonicData():
     while True:
-    try:
         GPIO.setmode(GPIO.BOARD)
         #assign gpio pin numbers to trigger&echo
         PIN_TRIGGER = 13
         PIN_ECHO = 11
-        prevDist = 0
 
         #assign trigger&echo to proper gpio i/o status
         GPIO.setwarnings(False)
@@ -93,26 +93,38 @@ def getUltraSonicData():
         #calculate distance based on times. assume speed of sound to  be 17150 cm/s. round distance to 2 decimal places
         pulse_duration = pulse_end_time - pulse_start_time
         distance = round(pulse_duration * 17150, 2)
-        if(distance!=prevDist):
-            print("Distance:",distance,"cm")
-            prevDist=distance
+        if(abs(distance-prevUltraDist)>2):
+            UltrasonicDistance=distance
+            print("UltrasonicDistance:",UltrasonicDistance,"cm")
+            prevUltraDist=UltrasonicDistance
         else:
-            prevDist=distance
+            prevUltraDist=UltrasonicDistance
 
     #allow keyboard interrupt to stop program
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
-        break
 
 def getLightData():
     while True:
-	    print("Value: " + str(ldr.value))
+        #print("Value: " + str(ldr.value))
+        light = ldr.value
+        if(abs(light-prevLight)>0.2):
+            LightValue=light
+            prevLight=light
+        else:
+            prevLight=light
 
 if __name__ == '__main__':
-	try:
-		getTFminiData()
-	except:  
-		pi.bb_serial_read_close(RX)
-		pi.stop()
+    try:
+        while True:
+            getTFminiData()
+            getUltraSonicData()
+            getLightData()
+
+        if (UltrasonicDistance<= 10 | LidarDistance<= 10 | LightValue>0.4):
+            os.system("fswebcam -r 1280x720 image.jpg")
+
+
+
+    except:  
+        pi.bb_serial_read_close(RX)
+        pi.stop()
  
