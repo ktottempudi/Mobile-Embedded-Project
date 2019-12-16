04720781e30a2a773c3b0cd00d95f13cd95a0e64
Atul Srivastava <atulsriv3@gmail.com>
2019-12-15 20:13:57 -0500

    Can recieve Picture

--- AllSensors.py
+++ AllSensors.py
@@ -4,11 +4,12 @@ import pigpio
 import RPi.GPIO as GPIO
 import time
 import os
+import gpiozero
 from gpiozero import LightSensor, Buzzer
 
 
 #pi cam code
-import picamera
+
 # camera =picamera.PiCamera()
 # camera.resolution=(800,600)
 # camera.start_preview()
@@ -16,7 +17,7 @@ import picamera
 # camera.capture('snapshot.jpg',resize=(640,480))
 # camera.stop_preview()
 
-
+GPIO.setmode(GPIO.BOARD)
 ldr = LightSensor(4)
 imcount=0
 RX = 23 #lidar gpio 23 so pin 16
@@ -33,10 +34,22 @@ prevLidVal = 0
 prevLight = 0
 
 def getTFminiData():
+    print("lidar")
+    global LidarDistance
+    global LightValue
+    global UltrasonicDistance
+    global prevUltraDist
+    global prevLidVal
+    global prevLight
+
+    count = 0
+
     while True:
         #print("#############")
         #time.sleep(0.05)   #change the value if needed
         time.sleep(1)
+        print("in while")
+        count = count + 1
         (count, recv) = pi.bb_serial_read(RX)
         if count > 8:
             for i in range(0, count-9):
@@ -50,81 +63,130 @@ def getTFminiData():
                         dist = recv[i+2] + recv[i+3] * 256
                         #strength = recv[i+4] + recv[i+5] * 256
                         if(abs(dist-prevLidVal)>5):
+                            print("lidar if")
                             LidarDistance=dist
                             print(LidarDistance)
                             prevLidVal=dist
+                            return
                         else:
-                            prevLidVal=dist 
-                        imcount =imcount+1
-                        prev=distance
+                            print("lidar else")
+                            prevLidVal=dist
+
+                            if count > 100:
+                                return
+                        
+
+
                         #else:
                             # raise ValueError('distance error: %d' % distance) 
                         #i = i + 9
 
 def getUltraSonicData():
+    print("ultrasonic")
+    global LidarDistance
+    global LightValue
+    global UltrasonicDistance
+    global prevUltraDist
+    global prevLidVal
+    global prevLight
+
     while True:
-        GPIO.setmode(GPIO.BOARD)
-        #assign gpio pin numbers to trigger&echo
-        PIN_TRIGGER = 13
-        PIN_ECHO = 11
+        try:
+            GPIO.setmode(GPIO.BOARD)
+            #assign gpio pin numbers to trigger&echo
+            PIN_TRIGGER = 13
+            PIN_ECHO = 11
 
-        #assign trigger&echo to proper gpio i/o status
-        GPIO.setwarnings(False)
-        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-        GPIO.setup(PIN_ECHO, GPIO.IN)
+            #assign trigger&echo to proper gpio i/o status
+            GPIO.setwarnings(False)
+            GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+            GPIO.setup(PIN_ECHO, GPIO.IN)
 
-        #set trigger to low
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+            #set trigger to low
+            GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-        #sensor calibration/settle time
-        time.sleep(1.4)
+            #sensor calibration/settle time
+            time.sleep(1.4)
 
-        #set trigger to high
-        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-        time.sleep(0.00001)
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+            #set trigger to high
+            GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+            time.sleep(0.00001)
+            GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-        #condition to set start/stop time based on echo
-        while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-        while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+            #condition to set start/stop time based on echo
+            while GPIO.input(PIN_ECHO)==0:
+                pulse_start_time = time.time()
+            while GPIO.input(PIN_ECHO)==1:
+                pulse_end_time = time.time()
 
-        #calculate distance based on times. assume speed of sound to  be 17150 cm/s. round distance to 2 decimal places
-        pulse_duration = pulse_end_time - pulse_start_time
-        distance = round(pulse_duration * 17150, 2)
-        if(abs(distance-prevUltraDist)>2):
-            UltrasonicDistance=distance
-            print("UltrasonicDistance:",UltrasonicDistance,"cm")
-            prevUltraDist=UltrasonicDistance
-        else:
-            prevUltraDist=UltrasonicDistance
+            #calculate distance based on times. assume speed of sound to  be 17150 cm/s. round distance to 2 decimal places
+            pulse_duration = pulse_end_time - pulse_start_time
+            distance = round(pulse_duration * 17150, 2)
+            if(abs(distance-prevUltraDist)>2):
+                UltrasonicDistance=distance
+                print("UltrasonicDistance:",UltrasonicDistance,"cm")
+                prevUltraDist=UltrasonicDistance
+                break
+            else:
+                prevUltraDist=UltrasonicDistance
+                break
 
+        except:
+            print("stopped")
+
+            break
     #allow keyboard interrupt to stop program
 
 def getLightData():
+    print("light")
+    global LidarDistance
+    global LightValue
+    global UltrasonicDistance
+    global prevUltraDist
+    global prevLidVal
+    global prevLight
+
     while True:
         #print("Value: " + str(ldr.value))
+        print("in Light While")
         light = ldr.value
         if(abs(light-prevLight)>0.2):
+            print("in Light IF")
             LightValue=light
             prevLight=light
+            break
         else:
             prevLight=light
+            break
+
+def main():
+
+    global LidarDistance
+    global LightValue
+    global UltrasonicDistance
+    global prevUltraDist
+    global prevLidVal
+    global prevLight
+    print("in main")
+
+    while True:
+        print("Beginning of while Main")
+        getTFminiData()
+        getUltraSonicData()
+        getLightData()
+        GPIO.cleanup()
+        print("in Main while")
+        print("Still in main While")
+        if (UltrasonicDistance<= 10 or LidarDistance<= 10 or LightValue>0.4):
+            print("evaluation")
+            os.system("fswebcam -r 1280x720 image.jpg")
+        print("end")
 
 if __name__ == '__main__':
     try:
-        while True:
-            getTFminiData()
-            getUltraSonicData()
-            getLightData()
+        print("Entering main")
+        main()
 
-        if (UltrasonicDistance<= 10 | LidarDistance<= 10 | LightValue>0.4):
-            os.system("fswebcam -r 1280x720 image.jpg")
-
-
-
-    except:  
+    except:
         pi.bb_serial_read_close(RX)
-        pi.stop()
- 
+        pi.stop() 
